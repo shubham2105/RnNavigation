2758057735abfab999929ebcdc562f294948c42e
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-17 11:50:53 +0530

    installed needed libraries and configured babel and metro files accordingly

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { SafeAreaView, StyleSheet, Text} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.headerText}>RnNavigationDemo</Text>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -14,10 +14,11 @@ export default App;
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    alignItems: 'center',
+    paddingVertical:50
   },
   headerText:{
     fontSize:24,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,13 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+// Get default Metro configuration
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Define your Metro configuration
+const config = mergeConfig(defaultConfig, {});
+
+// Apply Reanimated Metro configuration
+module.exports = wrapWithReanimatedMetroConfig(config);
